8cf2c134c127234df4d22a10f68a55cdd58f1b22
Antonio Abraham <tony.calablava@gmail.com>
2019-09-17 17:01:32 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,11 @@
-## code your solution here. 
+
+#define class Cat
+class Cat
+    attr_accessor :name
+    attr_reader :name
+    attr_writer :name
+    def meow
+        puts "meow!"
+    end
+end
+
