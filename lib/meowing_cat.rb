631eda8255611e2ad865999bf1e01882ce86949a
Tiffany Abraham <tony.calablaval@gmail.com>
2020-09-03 04:46:22 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -4,8 +4,15 @@ class Cat
     attr_accessor :name
     attr_reader :name
     attr_writer :name
+
+
     def meow
         puts "meow!"
     end
 end
 
+maru = Cat.new
+
+maru.name = "Maru"
+
+maru.meow
